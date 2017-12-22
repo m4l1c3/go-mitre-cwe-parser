d2f364d552f4aa9d1953bde113a1d72210de5618
m4l1c3 <jon@jonsomnia.com>
2017-12-22 10:23:29 -0700

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
-package gomiteparser
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	xj "github.com/basgys/goxml2json"
+)
 
 func main() {
+	xml := strings.NewReader("")
 
+	json, err := xj.Convert(xml)
+	if err != nil {
+		fmt.Printf("Error converting xml to json: %s\n", err)
+	}
+
+	fmt.Printf("JSON output: %s\n", json)
 }
